9560aa4dc9a29fcadb2fdbd3d41c010be8eae984
MoretS <s.moret@ecole-ipssi.net>
2021-10-06 16:01:37 +0200

    Add ASSET_HOST as an environment variable

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module DevelopmentApp
 
     # This needs to be set for correct images URLs in emails
     # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
-    # config.action_mailer.asset_host = "https://your.server.url"
+    config.action_mailer.asset_host = "https://#{Rails.application.secrets[:asset_host]}/" if Rails.application.secrets[:asset_host].present?
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
